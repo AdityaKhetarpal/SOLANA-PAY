77d53aad08d4de2ba12a7ceb83616307907970e5
Aditya Khetarpal <80028073+AdityaKhetarpal@users.noreply.github.com>
2023-02-16 18:21:24 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,66 @@
+import React, { useEffect, useState } from "react";
+import Product from "../components/Product";
+import dynamic from "next/dynamic";
+
+import { useWallet } from "@solana/wallet-adapter-react";
+
+const WalletMultiButtonDynamic = dynamic(
+  async () =>
+    (await import("@solana/wallet-adapter-react-ui")).WalletMultiButton,
+  { ssr: false }
+);
+
+// Constants
+const TWITTER_HANDLE = "_buildspace";
+const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
+
+const App = () => {
+  const { publicKey } = useWallet();
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    if (publicKey) {
+      fetch(`/api/fetchProducts`)
+        .then((response) => response.json())
+        .then((data) => {
+          setProducts(data);
+          console.log("Products", data);
+        });
+    }
+  }, [publicKey]);
+
+  const renderNotConnectedContainer = () => (
+    <div className="button-container">
+      <WalletMultiButtonDynamic className="cta-button connect-wallet-button" />
+    </div>
+  );
+
+  const renderItemBuyContainer = () => (
+    <div className="products-container">
+      {products.map((product) => (
+        <Product key={product.id} product={product} />
+      ))}
+    </div>
+  );
+
+  return (
+    <div className="App">
+      <div className="container">
+        <header className="header-container">
+          <p className="header"> 😳 Solana Emoji Store 😈</p>
+          <p className="sub-text">
+            The only emoji store that accepts Cryptocoins
+          </p>
+        </header>
+
+        <main>
+          {publicKey ? renderItemBuyContainer() : renderNotConnectedContainer()}
+        </main>
+
+        
+      </div>
+    </div>
+  );
+};
+
+export default App;
